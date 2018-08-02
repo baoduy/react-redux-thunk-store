1fc3e1ca7d1abc058dc9efde8ce7fef95e12e4f5
Steven <hoangbaoduy@gmail.com>
2018-08-02 21:31:33 +0800

    Update dev.js

--- src/store/dev.js
+++ src/store/dev.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-export default function configureStore(rootReducer, initialState, middleWares) {
+export default function configureStore(rootReducer, initialState={}, middleWares=[]) {
   return createStore(
     rootReducer,
     initialState,
